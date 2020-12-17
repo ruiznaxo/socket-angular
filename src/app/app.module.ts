13cdf25595b9a7b315c22223ccde4e7df3257c6e
Ignacio <ruiznaxo@gmail.com>
2020-12-17 15:36:41 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
+//Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
+const config: SocketIoConfig = { 
+  url: environment.webSocketUrl, options: {} 
+};
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [FooterComponent]
 })
 export class AppModule { }
